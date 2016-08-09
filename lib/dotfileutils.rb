c4ea422f73602d887615cc3ca8e0b85e45334349
Jacob Killelea <jkillelea@protonmail.ch>
2016-08-09 00:17:42 -0700

    DEPRECATED FILE - TO BE REMOVED

--- lib/dotfileutils.rb
+++ lib/dotfileutils.rb
@@ -1,3 +1,11 @@
+=begin
+THIS FILE IS DEPRECATED IN FAVOR OF EVERYTHING BEING CONTAINED IN THE dotfile.rb CLASS FILE
+DO NOT USE THIS MODULE. DO NOT EXPECT UPDATES. DO NOT EXPECT FUNCTIONALITY
+THIS FILE WILL BE REMOVED IN FUTURE VERSIONS
+COPYRIGHT JACOB KILLELEA <jkillelea@protonmail.ch>
+MIT LICENSE
+=end
+
 module DotFileUtils
 
   def self.basename(filepath)
@@ -8,7 +16,7 @@ module DotFileUtils
     File.absolute_path filename
   end
 
-  def self.is_dotted? # true if file begins with a '.'
+  def self.is_dotted?(basename) # true if file begins with a '.'
     if basename[0] == '.'
       true
     else
@@ -17,11 +25,19 @@ module DotFileUtils
   end
 
   def self.undot(basename) # removes a preceeding dot from the file name, if there is one
+    if is_dotted? basename
       return basename[1...basename.length]
+    else
+      return basename
+    end
   end
 
   def self.dot(basename) # adds a preceeding dot, if there isn't one
+    if is_dotted? basename
+      return basename
+    else
       return ".#{basename}"
+    end
   end
 
 end
