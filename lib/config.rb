2c2d9cea064cd143cb7df53a19ef485e642d8c4c
Jacob Killelea <Jkillelea@protonmail.ch>
2016-10-28 22:55:13 -0600

    wrapped options into a module

--- lib/config.rb
+++ lib/config.rb
@@ -1,25 +1,25 @@
-Config = { # hold config values (keys are same as those in Options hash)
-  :verbose => false
-}
-Options = { # available options, and their key (keys are same as those in Config hash)
-  :verbose => ['-v', '--verbose']
-}
+module Config
+  Settings = { # hold config values (keys are same as those in Options hash)
+    :verbose => false
+  }
+  Options = { # available options, and their key (keys are same as those in Settings hash)
+    :verbose => ['-v', '--verbose']
+  }
 
-# parse options, if a command line arg is one of the available switches, set relevant Config parameter to true
-def parse_opts(arg_array)
-  arg_array.each do |arg|
-    Options.each_with_index { |(key, val), index| # val is a string array
-      if val.include? arg
-        Config[key] = true
-      end
-    }
+  # parse options, if a command line arg is one of the available switches, set relevant Settings parameter to true
+  def self.parse_opts(arg_array)
+    arg_array.each do |arg|
+      Options.each_with_index { |(key, val), index| # val is a string array
+        if val.include? arg
+          Settings[key] = true
+        end
+      }
+    end
   end
-end
 
-def if_verbose(string)
-  if Config[:verbose]
-    puts string
-  else
-    return nil
+  def self.if_verbose(string)
+    if Settings[:verbose]
+      puts string
+    end
   end
 end
